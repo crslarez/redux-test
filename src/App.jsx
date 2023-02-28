7f1a025cdb53d3ac01fbb58cbf7aae958508930c
Carlos Salazar <crslarez@gmail.com>
2023-02-28 11:22:00 -0500

    Feat: axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
 import { Col } from 'antd'
 import Searcher from './components/Searcher'
-import PokemonCard from './components/PokemonCard'
 import PokemonList from './components/PokemonList'
+import logo from './statics/logo.svg'
 function App() {
 
   return (
     <div className="App">
+      <Col span={4} offset={10}>
+      <img src={logo} alt='Pokedux' />
+      </Col>
       <Col span={8} offset={8}>
       <Searcher />
       </Col>
